f26d9c6c6de9c4b4ba749be3446c0c0afd5a805d
metinkayali <nitemilayak@outlook.com>
2019-01-13 22:58:27 +0300

    Initial commit

---
+++ src/app/Program.fs
@@ -0,0 +1,31 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+open System.IO
+open FSharp.Data
+
+let home = System.Environment.ExpandEnvironmentVariables("%HOMEDRIVE%%HOMEPATH%")
+let data = Path.Combine(home, "bikahve", "data", "people.csv")
+
+type People = CsvProvider<"c:/repo/bikahve/data/people.csv">
+
+let employee = People.Load(data)
+let rows = Array.ofSeq employee.Rows
+
+let rand = new System.Random()
+
+let swap (a: _[]) x y =
+    let tmp = a.[x]
+    a.[x] <- a.[y]
+    a.[y] <- tmp
+
+// shuffle an array (in-place)
+let shuffle a =
+    Array.iteri (fun i _ -> swap a i (rand.Next(i, Array.length a))) a
+
+[<EntryPoint>]
+let main argv =
+    shuffle rows
+    let pairs = List.chunkBySize 2 (List.ofSeq rows)
+    printf "%A" pairs
+    0 // return an integer exit code
